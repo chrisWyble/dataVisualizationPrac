e70db2a65f54ad02fed1cb302d1b6de402fc3340
Chris Wyble <cwgiant@gmail.com>
2020-10-18 06:46:29 -0500

    Optimized the code with pandas

--- reader.py
+++ reader.py
@@ -1,27 +1,15 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 import csv
 
 myDict = {}
 
-
 with open('alabamaCovid19Data_10_13_2020.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    col_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            for k in row:
-                myDict[str(k)] = []
-            line_count += 1
-        for key in myDict.keys():
-            myDict[key].append(row[col_count])
-            col_count += 1
-        col_count = 0
-    
-    for k in myDict:
-        myDict[k] = myDict[k][1:]
+    dataFrame = pd.read_csv(csv_file) # makes a dataframe 
+    col_names = list(dataFrame.columns.values) # gets the column names
+    myDict = {col: list(dataFrame.loc[: , col]) for col in col_names} # .loc gets the row or col by name not index
     print(myDict)
-    
+
     
 #plt.plot()
 #plt.show()
